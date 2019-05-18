198f924fa58acce2eb23606c1b50cbaefd39df64
brunogeek9 <brunogeek9@gmail.com>
2019-05-18 17:03:40 -0300

    adicionado o componente da receita e do react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
 const API_KEY = "2e516a50ee82c5863e06471e67bdde29";
 
 class App extends Component {
@@ -11,7 +13,7 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=4`); 
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`); 
     const data = await api_call.json();
     //guardando a chamada de data no estado da aplicação
     this.setState({ recipes : data.recipes });
@@ -25,6 +27,7 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Form = props => (
-    <form onSubmit={props.getRecipe}>
-        <input type="text" name="recipeName"/>
+    <form onSubmit={props.getRecipe} style={{marginBottom:"2rem"}}>
+        <input className="form__input" type="text" name="recipeName"/>
 
-        <button>
+        <button className="form__button">
             Search
         </button>
     </form>
