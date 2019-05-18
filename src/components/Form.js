04ca7baa9eeff67db6079f15b76e0bc5a231a33d
brunogeek9 <brunogeek9@gmail.com>
2019-05-18 13:52:40 -0300

    criando o componente do formulario

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => (
+    <form>
+        <input type="text" />
+        <button>
+            Searc
+        </button>
+    </form>
+);
+
+export default Form;
